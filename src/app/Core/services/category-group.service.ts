c7d79fc68098f80e96a2fd97d3106cdffd13599c
Zer093 <105790577+Zer093@users.noreply.github.com>
2024-10-30 20:27:56 +0300

    Category Group

--- src/app/Core/services/category-group.service.ts
+++ src/app/Core/services/category-group.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/layout.service';
 import { HttpClientService } from './http-client.service';
-import { CategoryGroupResponse } from 'src/app/modules/category-group/category-group.module';
+import { CategoryGroupResponse, CategoryGroupRequest, CategoryGroupSearchRequest, CategoryGroupUpdateRequest } from 'src/app/modules/category-group/category-group.module';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,32 @@ export class CategoryGroupService {
   public SelectedData: CategoryGroupResponse | null = null;
   public Dialog: any | null = null;
   constructor(public layoutService: LayoutService, public httpClient: HttpClientService) { }
+  async Add(data: CategoryGroupRequest) {
+    const apiUrl = `/api/categoryGroup`;
 
- 
+    return await this.httpClient.post(apiUrl, data);
+  }
+
+  async Update(data: CategoryGroupUpdateRequest) {
+
+    const apiUrl = `/api/categoryGroup`;
+    return await this.httpClient.put(apiUrl, data);
+  }
+
+  async Delete(uuid: string) {
+
+    const apiUrl = `/api/categoryGroup/${uuid}`;
+    return await this.httpClient.delete(apiUrl, uuid)
+
+  }
+
+  async Search(filter: CategoryGroupSearchRequest) {
+
+    const apiUrl = `/api/categoryGroup/list?${this.layoutService.Filter(filter)}`;
+
+    return await this.httpClient.get(apiUrl)
+
+  }
+
+
 }
--- src/app/modules/category-group/category-group.module.ts
+++ src/app/modules/category-group/category-group.module.ts
@@ -5,8 +5,9 @@ import { CategoryGroupRoutingModule } from './category-group-routing.module';
 import { CategoryGroupComponent } from './category-group/category-group.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ResponseBase } from 'src/app/shared/class/class';
+import { ResponseBase, RequestBase, SearchRequestBase } from 'src/app/shared/class/class';
 import { AddCategoryGroupComponent } from './add-category-group/add-category-group.component';
+import { CategoryResponse } from '../categories/categories.module';
 
 
 @NgModule({
@@ -24,6 +25,43 @@ import { AddCategoryGroupComponent } from './add-category-group/add-category-gro
 export class CategoryGroupModule { }
 export interface CategoryGroupResponse extends ResponseBase {
 
+  uuid?: string;
+  categoryGroupTranslation?: { [key: string]: CategoryGroupTranslationResponse };
+  image: string,
+  categoryIDFK: string,
+  category: CategoryResponse
 
+}
+export interface CategoryGroupSearchRequest extends SearchRequestBase {
+  uuid?: string;
+  name: string;
+  categoryIDFK: string,
+  includeCategory: string
 
+}
+export interface CategoryGroupUpdateRequest extends RequestBase {
+  categoryGroupTranslation?: CategoryGroupTranslationRequest[];
+  image: string,
+  categoryIDFK: string,
+}
+
+export interface CategoryGroupRequest extends RequestBase {
+  categoryGroupTranslation?: CategoryGroupTranslationRequest[];
+  image: string,
+  categoryIDFK: string,
+}
+
+export interface CategoryGroupTranslationResponse {
+  uuid?: string;
+  name?: string;
+  language?: string;
+}
+export interface CategoryGroupTranslationRequest {
+  uuid?: string;
+  name?: string;
+  language?: string;
+}
+export interface CategoryGroupTranslationUpdateRequest {
+  uuid?: string;
+  name?: string;
 }
